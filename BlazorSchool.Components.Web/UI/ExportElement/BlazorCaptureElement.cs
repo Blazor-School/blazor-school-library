a59cbb54f3ec1e33368af22d9c8b20368d78c93a
Red Wei <hevilhuy2@gmail.com>
2022-11-11 23:29:37 +0700

    Added BlazorCaptureElement component.

---
+++ BlazorSchool.Components.Web/UI/ExportElement/BlazorCaptureElement.cs
@@ -0,0 +1,6 @@
+﻿namespace BlazorSchool.Components.Web.UI.ExportElement;
+public class BlazorCaptureElement
+{
+    // The component can import external CSS files.
+    // Use this link to research https://stackoverflow.com/questions/18191893/generate-pdf-from-html-in-div-using-javascript
+}
