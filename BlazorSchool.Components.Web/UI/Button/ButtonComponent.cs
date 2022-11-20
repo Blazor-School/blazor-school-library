703cbfb03f35aadaeed009b1f8806b7a8ea10834
Paweł Przytuła <pprzytula@arpideas.pl>
2022-11-20 21:47:45 +0100

    Added button component

---
+++ BlazorSchool.Components.Web/UI/Button/ButtonComponent.cs
@@ -0,0 +1,37 @@
+﻿using BlazorSchool.Components.Web.Core.Tokenize;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorSchool.Components.Web.UI.Button;
+public class ButtonComponent : ComponentBase
+{
+    [Parameter]
+    public bool IsButtonVisible { get; set; } = false;
+
+    [Parameter]
+    public RenderFragment? ChildContent { get; set; }
+
+    [Parameter(CaptureUnmatchedValues = true)]
+    public IReadOnlyDictionary<string, object>? AdditionalAttributes { get; set; }
+
+    [Parameter]
+    public EventCallback OnButtonClick { get; set; }
+
+    protected override void BuildRenderTree(RenderTreeBuilder builder)
+    {
+        if (IsButtonVisible)
+        {
+            builder.OpenElement(0, "button");
+            builder.AddAttribute(1, "type", "button");
+            builder.AddMultipleAttributes(2, AdditionalAttributes);
+            builder.AddAttribute(3, "onclick", OnButtonClick);
+            builder.AddContent(4, ChildContent);
+            builder.CloseElement();
+        }
+    }
+}
